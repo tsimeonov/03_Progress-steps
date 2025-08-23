c2345544da629d0d31f975b61ed9b4bd5c87be8e
tsimeonov <tsimeonov12@gmail.com>
2025-08-23 14:27:31 +0200

    Update app.js

---
+++ app.js
@@ -0,0 +1,4 @@
+const progress = document.querySelector('#progress');
+const prev = document.querySelector('#prev');
+const next = document.querySelector('#next');
+const circles = document.querySelector('.circles');
